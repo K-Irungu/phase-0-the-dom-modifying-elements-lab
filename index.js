e33d3a4f6e919a0e213f73510161a33db4bc99de
Kevin Irungu <kevin.irungu@student.moringaschool.com>
2022-08-14 23:57:50 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+const element = document.getElementById("main"); //call on the element with id 'main'
+element.remove(); //code to remove entrie element
+
+//Create a 'newHeader variable, with id=victory, and append it to teh body section.
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Kevin is the champion";
+document.body.append(newHeader);
+
+
+
+
